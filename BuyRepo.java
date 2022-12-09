08b67db2c77081acb9be80bacf31756a2a4fd5a6
vazurkargayatri <115343230+vazurkargayatri@users.noreply.github.com>
2022-12-09 19:00:15 +0530

    Add files via upload

---
+++ BuyRepo.java
@@ -0,0 +1,7 @@
+package com.gayatri;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BuyRepo extends JpaRepository<Buy, Integer> {
+
+}
---
+++ medicineRepo.java
@@ -0,0 +1,7 @@
+package com.akash;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface medicineRepo extends JpaRepository<medicine, Integer> {
+
+}
